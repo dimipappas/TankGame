d386cf2159ce3372810af045510848ea473e90fe
Dimitrios <dimpappas@me.com>
2020-02-24 21:36:25 +0100

    Creating of the project
    creating unreal engine project files

---
+++ TankSim/Source/TankSim/TankSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSIM_API ATankSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
